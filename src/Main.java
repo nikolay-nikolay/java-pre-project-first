53e9a7c0ca18e73755b433568cd4de2b168dcfbe
nikolay-nikolay <hgofee666@gmail.com>
2025-11-22 17:57:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+//TIP Чтобы <b>запустить</b> код, нажмите <shortcut actionId="Run"/> или
+// нажмите на значок <icon src="AllIcons.Actions.Execute"/> на полях.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
